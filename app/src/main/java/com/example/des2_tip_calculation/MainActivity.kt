f4f1f99b79494aca37b1266d638ddce180612ebd
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2024-04-04 08:38:04 -0300

    Implementação dos botões de taxas percentuais, função para pintar os botões conforme o clique do usuário, diminuição do tamanho do edittext.

--- app/src/main/java/com/example/des2_tip_calculation/MainActivity.kt
+++ app/src/main/java/com/example/des2_tip_calculation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.des2_tip_calculation
 
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -9,21 +10,32 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var percentual: TextInputEditText
     private lateinit var valor: TextInputEditText
     private lateinit var resultado: TextView
     private lateinit var qtd_p: TextView
     private lateinit var btnP: Button
     private lateinit var btnM: Button
+    private lateinit var btn1: Button
+    private lateinit var btn2: Button
+    private lateinit var btn3: Button
+    private lateinit var btn4: Button
+    private lateinit var btn5: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        percentual = findViewById(R.id.percent_E)
         valor = findViewById(R.id.valor_E)
         resultado = findViewById(R.id.resultado) //valor final do edittext
         btnP = findViewById(R.id.button_P)
         btnM = findViewById(R.id.button_M)
         qtd_p = findViewById(R.id.tv_qtd)
+        btn1 = findViewById(R.id.dez)
+        btn2 = findViewById(R.id.quinze)
+        btn3 = findViewById(R.id.vinte)
+        btn4 = findViewById(R.id.vincinco)
+        btn5 = findViewById(R.id.outros)
+
+        val botoes = listOf(btn1,btn2,btn3,btn4,btn5)
+        var perc: Float = 0.0f
         var qtdPessoas: Int = 1
         qtd_p.text = qtdPessoas.toString()
 
@@ -32,16 +44,15 @@ class MainActivity : AppCompatActivity() {
             override fun afterTextChanged(s: Editable?) {}
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                calculateAndDisplayResult(qtdPessoas) //Essa função serve pra LITERALMENTE calcular e mostrar o result
+                calculateAndDisplayResult(qtdPessoas, perc) //Essa função serve pra LITERALMENTE calcular e mostrar o result
             }
         }
-        percentual.addTextChangedListener(textwater)
         valor.addTextChangedListener(textwater)//Quando o texto é digitado esse método é chamado.
         btnM.setOnClickListener {
             if (qtdPessoas > 1) {
                 qtdPessoas--
                 qtd_p.text = qtdPessoas.toString()
-                calculateAndDisplayResult(qtdPessoas)
+                calculateAndDisplayResult(qtdPessoas, perc)
             } else {
                 //deixar o botão BRANCO
             }
@@ -49,18 +60,41 @@ class MainActivity : AppCompatActivity() {
         btnP.setOnClickListener {
             qtdPessoas++
             qtd_p.text = qtdPessoas.toString()
-            calculateAndDisplayResult(qtdPessoas)
+            calculateAndDisplayResult(qtdPessoas,perc)
+        }
+        btn1.setOnClickListener {
+            perc = 0.10f
+            pintar(btn1,botoes)
+            calculateAndDisplayResult(qtdPessoas,perc)
+        }
+        btn2.setOnClickListener {
+            perc = 0.15f
+            pintar(btn2,botoes)
+            calculateAndDisplayResult(qtdPessoas,perc)
+        }
+        btn3.setOnClickListener {
+            perc = 0.20f
+            pintar(btn3,botoes)
+            calculateAndDisplayResult(qtdPessoas,perc)
+        }
+        btn4.setOnClickListener {
+            perc = 0.25f
+            pintar(btn4,botoes)
+            calculateAndDisplayResult(qtdPessoas,perc)
+        }
+        btn5.setOnClickListener {
+            perc = 0.30f
+            pintar(btn5,botoes)
+            calculateAndDisplayResult(qtdPessoas,perc)
         }
     }
 
-    private fun calculateAndDisplayResult(qtd: Int) {
-        var percentuall = percentual.text.toString().toFloatOrNull()
+    private fun calculateAndDisplayResult(qtd: Int, per: Float) {
+        var percentuall = per
         val valorr = valor.text.toString().toFloatOrNull()
         val pessoass = qtd.toFloat()
 
-        if (percentuall != null && valorr != null && pessoass != null) {
-            percentuall = percentuall / 100
-
+        if (valorr != null) {
             var valor_conta: String = ""
             var valor_gorjeta: String = ""
             var valor_total: String = ""
@@ -83,7 +117,22 @@ class MainActivity : AppCompatActivity() {
             resultado.text =
                 "R$ $valor_conta\n\nR$ $valor_gorjeta\n\nR$ $valor_total"
         } else {
-            resultado.text = ""
+            resultado.text = "R$ 00,00\n\nR$ 00,00\n\nR$ 00,00"
         }
     }
+}
+
+private fun pintar(btnSel:Button, todos:List<Button>){
+
+todos.forEach{botao ->
+    if (botao == btnSel){
+        botao.setBackgroundColor(Color.GREEN)
+    }else{
+        botao.setBackgroundColor(Color.MAGENTA)
+    }
+}
+
+
+
+
 }
